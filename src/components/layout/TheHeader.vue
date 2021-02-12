608a9720ff6cc654603ad271b563a5e5bd733d6b
Haniel <hhernandez@aimservices.tech>
2021-02-11 19:23:52 -0600

    Tempalte started

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export class TheHeaderComponent extends Vue {
+
+}
+</script>
+<style lang="scss">
+
+</style>
