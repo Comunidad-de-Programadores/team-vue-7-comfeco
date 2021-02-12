19de11c0b4217041621f5096ddb17a62dc4aa125
Haniel <hhernandez@aimservices.tech>
2021-02-11 19:23:44 -0600

    Template Started

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <router-view/>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component(
+  {
+    components: {
+
+    }
+  }
+)
+export default class App extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
