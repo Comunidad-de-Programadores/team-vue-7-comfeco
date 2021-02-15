fa83fbab0054f597043fbc9096e52435515f16d9
hmurillo <heber_jose@hotmail.com>
2021-02-14 19:16:00 -0600

    Form forgot password ready

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -21,8 +21,8 @@ export class Auth {
    * Sen email to reset password
    * @param data
    */
-  forgotPassword (data: AuthRequest) {
-    console.log(data)
+  forgotPassword (email: string): Promise<void> {
+    return firebaseAuth.sendPasswordResetEmail(email)
   }
 
   /**
--- src/pages/auth/ForgotPassword.vue
+++ src/pages/auth/ForgotPassword.vue
@@ -10,20 +10,35 @@
           <b-input type="email" v-model="email"></b-input>
         </b-field>
       </ValidationProvider>
-      </ValidationObserver>
+      <div class="buttons">
+         <button class="button is-success" @click="handleSubmit(submit)">
+          <span class="icon is-small">
+            <i class="fas fa-check"></i>
+          </span>
+          <span>Recuperar Contraseña</span>
+        </button>
+      </div>
+    </ValidationObserver>
   </div>
 </template>
 <script lang="ts">
 import { ValidationObserver, ValidationProvider } from 'vee-validate'
 import { Vue, Component } from 'vue-property-decorator'
+import Auth from '@/auth/auth.ts'
 @Component({
   components: { ValidationProvider, ValidationObserver }
 })
 export default class ForgotPassword extends Vue {
-      email ='';
+  email = '';
+  async submit () {
+    try {
+      await Auth.forgotPassword(this.email)
+      console.log('Email send')
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
-
 </script>
 <style lang="">
-
 </style>
