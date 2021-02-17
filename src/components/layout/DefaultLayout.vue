13fbefbef03be70d22e86fdc0ba4d5fbe4820e2b
Haniel <hhernandez@aimservices.tech>
2021-02-16 19:10:56 -0600

    Home layout added

---
+++ src/components/layout/DefaultLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <TheHeader/>
+    <slot></slot>
+    <TheFooter/>
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+import TheHeader from "./TheHeader.vue";
+import TheFooter from "./TheFooter.vue";
+
+@Component({
+  components:{
+    TheHeader,
+    TheFooter
+  }})
+export default class DefaultLayout extends Vue {
+
+  @Prop 
+}
+</script>
+
+<style></style>
