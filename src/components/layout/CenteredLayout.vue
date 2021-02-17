d4bcc8fcaac4ef29a563cf5295c103ef1567d575
Haniel <hhernandez@aimservices.tech>
2021-02-17 07:48:52 -0600

    Layotu centered added

---
+++ src/components/layout/CenteredLayout.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div>
+    
+  </div>
+</template>
+<script>
+import {Vue, Component} from 'vue-property-decorator'
+@Component
+export default class ForgotPassword extends Vue {
+}
+</script>
+<style lang="">
+  
+</style>
