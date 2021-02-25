3538d6dc48613dd92bbe995f18024e42aa0ecafa
Haniel <hhernandez@aimservices.tech>
2021-02-24 19:44:23 -0600

    Improved clock

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,61 +1,63 @@
 <template lang="html">
-    <p v-if="currentTime.total" class="title is-1">
-      {{ `${currentTime.days} días ${currentTime.hours}:${currentTime.minutes}:${currentTime.seconds}` }}
-    </p>
+  <p v-if="currentTime.total" class="title is-1">
+    {{
+      `${currentTime.days} días ${currentTime.hours}:${currentTime.minutes}:${currentTime.seconds}`
+    }}
+  </p>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 
-interface Time{
-  total?:number,
-  days?:number,
-  hours?:number,
-  minutes?:number,
-  seconds?:number,
-
+interface Time {
+  total?: number;
+  days?: number;
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
 }
 
-@Component({
-
-})
+@Component({})
 export default class ForgotPassword extends Vue {
-currentTime:Time = {
-  total: 0,
-  days: 0,
-  hours: 0,
-  minutes: 0,
-  seconds: 0
-};
+  // VARIABLES
+  currentTime: Time = {
+    total: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  };
 
-@Prop({ type: String, required: true }) readonly deadline!: string;
-@Prop({ type: Number, required: true }) readonly speed!: number;
+  @Prop({ type: String, required: true }) readonly deadline!: string;
+  @Prop({ type: Number, required: true }) readonly speed!: number;
+  /**
+   * Mounted method
+   */
+  mounted (): void {
+    setTimeout(this.countdown, 1000)
+  }
 
-mounted ():void {
-  console.log('CLOCK COMPONENT MOUNTED')
-  setTimeout(this.countdown, 1000)
-}
-
-countdown ():void {
-  const t = Date.parse(this.deadline) - new Date().getTime()
-  const seconds = Math.floor((t / 1000) % 60)
-  const minutes = Math.floor((t / 1000 / 60) % 60)
-  const hours = Math.floor((t / (1000 * 60 * 60)) % 24)
-  const days = Math.floor(t / (1000 * 60 * 60 * 24))
-  if (t > 0) {
-    this.currentTime = {
-      total: t,
-      days: days,
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds
+  /**
+   * Creates a count down for the dealine
+   */
+  countdown (): void {
+    const t = Date.parse(this.deadline) - new Date().getTime()
+    const seconds = Math.floor((t / 1000) % 60)
+    const minutes = Math.floor((t / 1000 / 60) % 60)
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24)
+    const days = Math.floor(t / (1000 * 60 * 60 * 24))
+    if (t > 0) {
+      this.currentTime = {
+        total: t,
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+      }
+      setTimeout(this.countdown, this.speed)
+    } else {
+      this.currentTime = {}
     }
-    setTimeout(this.countdown, this.speed)
-  } else {
-    this.currentTime = {}
   }
 }
-}
 </script>
-<style lang="">
-
-</style>
+<style lang=""></style>
