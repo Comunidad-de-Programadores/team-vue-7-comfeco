628b63942b7263bf8c01e5f1589a6e8bf5cdcb37
Haniel <hhernandez@aimservices.tech>
2021-03-02 08:04:52 -0600

    improements

--- src/components/tabs/profile/ProfileTabEvents.vue
+++ src/components/tabs/profile/ProfileTabEvents.vue
@@ -1,30 +1,28 @@
 <template lang="HTML">
-  <div class="hero-head py-1 has-text-centered">
-    <nav class="navbar">
-      <div class="navbar-menu is-justify-content-space-between">
-          <a href="" class="navbar-item is-active has-text-left">
-            Eventos de tu interes
+  <div class="block">
+    <div class="is-flex is-justify-content-space-between">
+        <h4 class="title is-4">
+          Eventos de tu interes
+        </h4>
+          <a href="" class="">
+            Ver más
           </a>
-          <a href="" class="navbar-item is-active  ml-6 pl-3 has-text-right">
-            Apoyar Iniciativa
-          </a>
-      </div>
-    </nav>
-    <div class="container py-6 ">
-      <figure class="image is-3by1">
-        <img src="https://bulma.io/images/placeholders/128x128.png">
-      </figure>
     </div>
     <div class="card">
-  <div class="card-content">
-    <p class="py-3 has-text-left">Community fest and code</p>
-    <div class="content is-inline-flex ">
-      <a href="#" class="button is-rounded">Abandonar</a>
-      <a href="#" class="button is-rounded ml-3">Mas Informacion</a>
-    </div>
+      <div class="card-image">
+        <figure class="image is-4by3">
+      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+    </figure>
+      </div>
+      <div class="card-content">
+        <p class="title is-4">Community fest and code</p>
+        <div class="content is-inline-flex ">
+          <a href="#" class="button ">Abandonar</a>
+          <a href="#" class="button ml-3">Mas Informacion</a>
+        </div>
+  </div>
   </div>
 </div>
-  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
