f9079da2696b73193f06921e7ec674e62db97abc
Haniel <hhernandez@aimservices.tech>
2021-03-10 20:10:30 -0600

    Improvements  on recent Actitvityes

--- src/components/tabs/profile/RecentActivities.vue
+++ src/components/tabs/profile/RecentActivities.vue
@@ -4,19 +4,13 @@
     <p class="title is-4 mb-1 has-text-weight-bold">Actividad Reciente </p>
   </div>
   <div class="column is-12 mb-3"  v-for="item in activities" >
-      <div class="box">
-        <div class="columns">
-          <div class="column is-10">
-            <p class="subtitle is-5">
-            {{item.evento}}
-            </p>
-          </div>
-           <div class="column is-2">
-              <figure class="image is-64x64">
-                <img src='https://bulma.io/images/placeholders/128x128.png' alt="Image">
-              </figure>
-          </div>
-        </div>
+      <div class="box is-flex is-justify-content-space-between is-align-items-center">
+        <p class="subtitle is-5 mb-0">
+          {{item.evento}}
+        </p>
+        <figure class="image is-64x64">
+          <img src='https://bulma.io/images/placeholders/128x128.png' alt="Image">
+        </figure>
       </div>
   </div>
 </div>
