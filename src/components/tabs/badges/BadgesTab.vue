57a854feb7356a5edd4cb2ed8c590a1decee274d
Haniel <hhernandez@aimservices.tech>
2021-03-13 19:42:40 -0600

    Badges improved

--- src/components/tabs/badges/BadgesTab.vue
+++ src/components/tabs/badges/BadgesTab.vue
@@ -1,18 +1,30 @@
 <template lang="">
   <div class="columns pt-6">
-    <div class="column is-4 pt-0" >
-       <ElementBadge />
+    <div class="column is-4 pt-0 " v-for="badge  in badges" :key="badge.id" >
+       <ElementBadge :badge="badge" :completed="badgeCompleted(badge)"  />
     </div>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Badge } from '@/models/Badge'
+import { HasUserInfo } from '@/models/HasUserInfo'
+import { Component } from 'vue-property-decorator'
 import ElementBadge from './ElementBadge.vue'
 @Component({
   components: {
     ElementBadge
   }
 })
-export default class BadgesTab extends Vue {}
+export default class BadgesTab extends HasUserInfo {
+  badges = [{
+    id: 1,
+    name: 'badge 1',
+    complete: false
+  }]
+
+  badgeCompleted (badge:Badge): boolean {
+    return this.currentUserInfo.badges ? this.currentUserInfo.badges.includes(badge.name) : false
+  }
+}
 </script>
 <style lang=""></style>
