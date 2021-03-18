7aa766bbee2b9af46b8761fcca6196469be8a4cf
Eden <gresseden@gmail.com>
2021-03-18 18:14:23 -0400

    adding objects

--- src/config/events.ts
+++ src/config/events.ts
@@ -2,5 +2,25 @@ export const EVENTS = [
   {
     name: 'event 1',
     imageUrl: 'http://lorempixel.com/400/200'
-  }
+  },
+  {
+    name: 'event 1',
+    imageUrl: 'http://lorempixel.com/400/200'
+  },
+  {
+    name: 'event 1',
+    imageUrl: 'http://lorempixel.com/400/200'
+  },
+  {
+    name: 'event 1',
+    imageUrl: 'http://lorempixel.com/400/200'
+  },
+  {
+    name: 'event 1',
+    imageUrl: 'http://lorempixel.com/400/200'
+  },
+  {
+    name: 'event 1',
+    imageUrl: 'http://lorempixel.com/400/200'
+  },
 ]
